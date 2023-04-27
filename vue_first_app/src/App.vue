7f2ed9ab93b6efc2a2349aae3dc849493720bb28
BlackShirt-Coder <haruta685@gmail.com>
2023-04-28 01:00:03 +0630

    first commit

---
+++ vue_first_app/src/App.vue
@@ -0,0 +1,105 @@
+<template>
+  <section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <ul>
+      <friend_contact
+      id='1112'
+        name="mgmg"
+        email-address="mgmg@gmail.com"
+        phone-number="09302302">
+      </friend_contact>
+      <friend_contact 
+      id='3232'
+      name="susu"
+      email-address='susu@gmail.com'
+      phone-number='049204202'
+      > 
+
+      </friend_contact>
+    </ul>
+  </section>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0390320302",
+          email: "manuel@gmail.com",
+        },
+        {
+          id: "cusor",
+          name: "peter cusor",
+          phone: "0249029302",
+          email: "petercusor@gmail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: "Jost", sans-serif;
+}
+body {
+  margin: 0;
+}
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
---
+++ vue_first_app/src/components/FriendContact.vue
@@ -0,0 +1,46 @@
+<template>
+  <section>
+    <center>
+    <h1>{{ name }}</h1>
+    <button @click="showDetails()">Show Details</button>
+    <ul v-if="detailsAreVisabled">
+      <li>{{ id }}</li>
+      <li>{{ name }}</li>
+      <li>{{ emailAddress }}</li>
+      <li>{{ phoneNumber }}</li>
+    </ul>
+    </center>
+  </section>
+</template>
+<script>
+export default {
+ props:[
+'id',
+'name',
+'emailAddress',
+'phoneNumber'
+ ],
+  data() {
+    return {
+    
+      detailsAreVisabled: false,
+      friends: 
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0390320302",
+          email: "manuel@gmail.com",
+        },
+        
+    
+    }
+},
+      methods: {
+        showDetails() {
+          this.detailsAreVisabled = !this.detailsAreVisabled;
+          console.log(this.friends);
+        },
+      }
+  
+}
+</script>
